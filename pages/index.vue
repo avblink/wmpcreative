d73277d6326b30a9e3b31e1ed83072057c582aa4
Alexander Benjamin <abenjamin@avblink.com>
2019-11-08 15:03:52 +0100

    - Just testing GraphQl with fake data

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,30 @@
   <section>
     <h1>Creative excellence.</h1>
     <h2>Designed. Developed. Delivered.</h2>
+    <div v-for="user in allUsers" :key="user.id">
+      {{ user.name }}: {{ user.id }}
+    </div>
   </section>
 </template>
+
+<script>
+// Tutorial: https://strapi.io/blog/cooking-a-deliveroo-clone-with-nuxt-vue-js-graphql-strapi-and-stripe-setup-part-1-7/
+import usersQuery from '~/apollo/queries/users/users'
+
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  apollo: {
+    allUsers: {
+      prefetch: true,
+      query: usersQuery,
+      variables () {
+        return { count: 3 }
+      }
+    }
+  }
+}
+</script>
